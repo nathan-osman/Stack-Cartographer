842f81bbda7c178caa14459b00c5de2c929937e0
Nathan Osman <nathan.osman@gmail.com>
2012-03-24 11:25:49 -0700

    Added code for parsing individual methods.

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 <h3>Usage</h3>
 <p>Just run the following command to generate the map:</p>
 <pre><code>php generate.php</code></pre>
+<p>This will generate a file <code>map.json</code> containing the methods and their parameters.</p>
--- generate.php
+++ generate.php
@@ -28,10 +28,13 @@ class Generator
     
     // Member variables
     private $curl_handle = null;
+    private $last_request = 0;  // timestamp of the last request
     
     // Initializes the generator
     function __construct()
     {
+        // Turn off errors and initialize cURL
+        libxml_use_internal_errors(TRUE);
         $this->curl_handle = curl_init();
     }
     
@@ -39,6 +42,11 @@ class Generator
     // or (if provided) executes an XPath query and returns the results
     private function FetchDOM($url, $query=null)
     {
+        // Slam on the brakes if we're making requests too fast
+        $diff = microtime(TRUE) - $this->last_request;
+        if($diff < 0.5)
+            usleep((0.5 - $diff) * 1000000);
+        
         curl_setopt($this->curl_handle, CURLOPT_URL, $url);
         curl_setopt($this->curl_handle, CURLOPT_USERAGENT, self::UserAgent);
         curl_setopt($this->curl_handle, CURLOPT_RETURNTRANSFER, 1);
@@ -51,7 +59,7 @@ class Generator
         
         // Construct a DOMDocument from the data we received and return it
         $document = new DOMDocument();
-        @$document->loadHTML($data);
+        $document->loadHTML($data);
         
         if($document === FALSE)
             throw new Exception('There was an error parsing the HTML retrieved.');
@@ -65,26 +73,104 @@ class Generator
             return $document;
     }
     
+    // Returns the expected item in the document if found and throws an exception if not found
+    private function FindInDocument($document, $query, $item)
+    {
+        $first_element = (new DOMXPath($document))->query($query);
+        
+        // Ensure at least one item was found
+        if($first_element === FALSE || !$first_element->length)
+            throw new Exception("Unable to locate $item in the DOM.");
+        
+        // Return the first item
+        return $first_element->item(0);
+    }
+    
     // Returns a list of all pages containing API methods
     private function GetMethodList()
     {
         // Fetch all of the '.method-name a' elements
-        $links = $this->FetchDOM(self::Site . '/docs', '//div[@class="method-name"]/a/@href');
+        $links = $this->FetchDOM(self::Site . '/docs', '//div[@class="method-name"]/a');
+        if(!$links->length)
+            throw new Exception('Unable to locate the method list in the DOM.');
         
         // Convert the results into an array and return it
         $urls = array();
         foreach($links as $link)
-            $urls[] = $link->nodeValue;
+            $urls[$link->nodeValue] = $link->attributes->getNamedItem('href')->nodeValue;
         
-        return array_unique($urls);
+        return $urls;
+    }
+    
+    // Returns specific details about the method
+    private function GetMethodDetails($name, $doc_url)
+    {
+        // Fetch the page with the details
+        $document = $this->FetchDom(self::Site . $doc_url);
+        
+        // Grab the description
+        $desc = $this->FindInDocument($document, '//div[@class="indented"]/p', 'the method description')->nodeValue;
+        
+        // Grab the <script> tag containing the parameters
+        $script = $this->FindInDocument($document, '//script[contains(.,"var parameters")]', 'the parameter script')->nodeValue;
+        if(preg_match('/var parameters = (.*?);/', $script, $matches))
+        {
+            $parameters = json_decode($matches[1], TRUE);
+            
+            // Contruct the JSON data to return
+            return array('path'        => $name,
+                         'description' => $desc,
+                         'parameters'  => $parameters);
+        }
+        else
+            throw new Exception('Unable to apply regular expression to parameter script.');
+    }
+    
+    // Generates the JSON that will be written to the output file
+    private function GenerateJSON($methods)
+    {
+        return json_encode(array('meta'    => array('generator' => self::UserAgent,
+                                                    'date'      => date(DATE_RFC850)),
+                                 'methods' => $methods),
+                           JSON_PRETTY_PRINT);
     }
     
     // Begins and manages the entire generation process
     public function Go()
     {
-        echo "Fetching list of all methods...\n";
-        $methods = $this->GetMethodList();
-        echo count($methods) . " methods found - parsing them one at a time...\n";
+        // Display copyright information
+        echo "Stack Cartographer\nCopyright 2012 - Nathan Osman\n\n";
+        
+        try
+        {
+            echo "Fetching list of all methods...\n";
+            $unparsed_methods = $this->GetMethodList();
+            echo count($unparsed_methods) . " methods found - parsing them one at a time...\n";
+            
+            // Parse each of the methods we found
+            $methods = array();
+            foreach($unparsed_methods as $name => $doc_url)
+            {
+                echo " - Parsing $name...\n";
+                $methods[] = $this->GetMethodDetails($name, $doc_url);
+            }
+            
+            // Open the output file...
+            echo "Writing results to output file...\n";
+            $f = @fopen('map.json', 'w');
+            if($f === FALSE)
+                throw new Exception('Unable to write to output file.');
+            
+            // ...and write the results
+            fwrite($f, $this->GenerateJSON($methods));
+            fclose($f);
+            
+            echo "Mapping complete!";
+        }
+        catch(Exception $e)
+        {
+            echo 'Error: ' . $e->getMessage();
+        }
     }
 }
 
